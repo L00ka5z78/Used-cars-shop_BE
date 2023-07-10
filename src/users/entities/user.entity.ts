5e42231ccf535df58f080e33f39226cd30244b10
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-07-10 15:41:32 +0200

    pseudo login logic added to check if it works 10.07.23 15:41

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,12 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsEmail } from 'class-validator';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -6,8 +14,22 @@ export class User {
   id: number;
 
   @Column()
+  // @IsEmail()
   email: string;
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user wit id', this.id);
+  }
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated user wit id', this.id);
+  }
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed user wit id', this.id);
+  }
 }
