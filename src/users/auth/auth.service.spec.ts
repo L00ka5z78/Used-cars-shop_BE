18c381b1857908631988022a1ca792538784e003
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-07-13 09:11:37 +0200

    basic testing setup added 13.07.23 09:11

---
+++ src/users/auth/auth.service.spec.ts
@@ -0,0 +1,15 @@
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from '../users.service';
+
+describe('AUTH CONTROLLER', () => {
+  it('can create an instance of auth service', async () => {
+    const module = await Test.createTestingModule({
+      providers: [AuthService],
+    }).compile();
+
+    const service = module.get(AuthService);
+
+    expect(service).toBeDefined();
+  });
+});
