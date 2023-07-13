ff636ed6cb04e26f5c8e9f190f02760bf7ec79bb
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-07-13 09:16:16 +0200

    first example  test added 13.07.23 09:16

--- src/users/auth/auth.service.spec.ts
+++ src/users/auth/auth.service.spec.ts
@@ -4,8 +4,26 @@ import { UsersService } from '../users.service';
 
 describe('AUTH CONTROLLER', () => {
   it('can create an instance of auth service', async () => {
+    //create fake copy of users service
+
+    const fakeUsersService = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({
+          id: 1,
+          email,
+          password,
+        }),
+    };
+
     const module = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
     }).compile();
 
     const service = module.get(AuthService);
