4d58e3df84bfd1d7930a05c6e23a16b81b3ce0d8
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-07-13 10:01:58 +0200

    test if user signs in with email in use added 13.07.23 10:01

--- src/users/auth/auth.service.spec.ts
+++ src/users/auth/auth.service.spec.ts
@@ -2,12 +2,14 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from '../users.service';
 import { User } from '../entities';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('AUTH service', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
+
   beforeEach(async () => {
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       findUserByEmail: () => Promise.resolve([]),
       createUser: (email: string, password: string) =>
         Promise.resolve({
@@ -43,5 +45,13 @@ describe('AUTH service', () => {
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.findUserByEmail = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    await expect(
+      service.signup('testing@purposes.email', 'testing'),
+    ).rejects.toThrow(BadRequestException);
   });
 });
