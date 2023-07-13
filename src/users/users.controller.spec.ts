600a2a1046866dc728ea54d5fae40dc991f64c76
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-07-13 11:20:01 +0200

    first basic unit test added 13.07.23 11:!9

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -10,11 +10,44 @@ describe('UsersController', () => {
   let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
-    fakeUsersService = {};
-    fakeAuthService = {};
+    fakeUsersService = {
+      findOneUserById: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'test@test.com',
+          password: '123abc',
+        } as User);
+      },
+      findUserByEmail: (email: string) => {
+        return Promise.resolve([
+          { id: 1, email, password: 'randompass' } as User,
+        ]);
+      },
+      // updateUser: (id: number, attrs: Partial<User>) => {
+      //   return Promise.resolve([{id: 1, email, password: 'randompass'} as User])
+
+      // },
+      removeUser: (id: number) => {
+        return Promise.resolve({ id } as User);
+      },
+    };
+    fakeAuthService = {
+      // signup: () => {},
+      // signin: () => {}
+    };
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
